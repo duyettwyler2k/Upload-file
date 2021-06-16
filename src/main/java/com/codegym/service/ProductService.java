a83250662360833def9c4e623b9cdb5bfde6374d
duyettwyler2k <duyettwyller@gmail.com>
2021-06-16 14:35:45 +0700

    'sda'

---
+++ src/main/java/com/codegym/service/ProductService.java
@@ -0,0 +1,45 @@
+package com.codegym.service;
+
+import com.codegym.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductService implements IProductService {
+    private List<Product> products = new ArrayList<>();
+
+    @Override
+    public List<Product> findAll() {
+        return products;
+    }
+
+    @Override
+    public void save(Product product) {
+        products.add(product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        return products.get(id);
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        for (Product product1 : products) {
+            if (product1.getId() == id) {
+                product1 = product;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getId() == id) {
+                products.remove(i);
+                break;
+            }
+        }
+    }
+}
